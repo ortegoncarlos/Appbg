6e63a6f964b211ca3cce8eef0285a22c0167d6cd
pulsarav <carlosortegon@taller.jbarretotransportes.local>
2014-07-28 09:38:25 -0500

    Camera

--- app.js
+++ app.js
@@ -48,15 +48,15 @@
                 encodingType: Camera.EncodingType.JPEG,
                 sourceType: Camera.PictureSourceType.CAMERA
             };
-        uploadPicture = function (e) {
-            var options = {
-                quality: 45,
-                targetWidth: 1000,
-                targetHeight: 1000,
-                destinationType: Camera.DestinationType.FILE_URI,
-                encodingType: Camera.EncodingType.JPEG,
-                sourceType: Camera.PictureSourceType.PHOTOLIBRARY
-            };    
+        // uploadPicture = function (e) {
+        //     var options = {
+        //         quality: 45,
+        //         targetWidth: 1000,
+        //         targetHeight: 1000,
+        //         destinationType: Camera.DestinationType.FILE_URI,
+        //         encodingType: Camera.EncodingType.JPEG,
+        //         sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+        //     };    
 
             navigator.camera.getPicture(
                 function (imageURI) {
